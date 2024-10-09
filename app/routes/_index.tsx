703bdc37f3e4ce824fe3cb84269c6c9976b51584
orztv <qi@outlook.cz>
2024-10-09 09:17:31 +0800

    update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -53,6 +53,14 @@ export default function Index() {
                 白嫖 CF 的 Flux 生成图片
               </Link>
             </li>
+            <li>
+              <Link
+                to="https://github.com/aigem/cf-flux-remix"
+                className="block w-full text-center px-6 py-3 text-lg font-semibold text-white bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+              >
+                访问 Github
+              </Link>
+            </li>
             {/* 可以在这里添加更多的导航项 */}
           </ul>
         </nav>
